1877234c45b086bef05e184dbfc4fa3038efd951
Amalia Khachatryan <amalia-khachatryan@mail.ru>
2023-03-30 21:56:04 +0300

    junit4-platform error

--- src/test/java/service/CashbackHackServiceTest.java
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -4,35 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
 
+    CashbackHackService service = new CashbackHackService();
+    @Test
+    public void remain5Test() {
+        Assert.assertEquals(0, service.remain(1000));
+    }
     @Test
     public void remain1Test() {
-        Assert.assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(1000, service.remain(0));
     }
-
     @Test
     public void remain2Test() {
-        Assert.assertEquals(service.remain(1), 999);
+        Assert.assertEquals(999, service.remain(1));
     }
-
     @Test
     public void remain3Test() {
-        Assert.assertEquals(service.remain(100), 900);
+        Assert.assertEquals(900, service.remain(100));
     }
-
     @Test
     public void remain7Test() {
-        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(100, service.remain(900));
     }
-
     @Test
     public void remain4Test() {
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(1, service.remain(999));
     }
-
     @Test
     public void remain6Test() {
-        Assert.assertEquals(service.remain(500), 500);
+        Assert.assertEquals(500, service.remain(500));
     }
 }
