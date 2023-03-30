67b657d2b7df4864d97dc587eff103425bade007
Amalia Khachatryan <amalia-khachatryan@mail.ru>
2023-03-30 21:46:16 +0300

    testng error

--- src/test/java/service/CashbackHackServiceTest.java
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-
+    @Test
+    public void remain5Test() {
+        Assert.assertEquals(service.remain(1000), 0);
+    }
     @Test
     public void remain1Test() {
         Assert.assertEquals(service.remain(0), 1000);
