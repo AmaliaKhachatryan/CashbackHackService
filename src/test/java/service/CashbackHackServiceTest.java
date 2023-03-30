46961ef89eca266512e0535f2f68ddc826d69e7c
Amalia Khachatryan <amalia-khachatryan@mail.ru>
2023-03-30 21:37:05 +0300

    junit4 Error

--- src/test/java/service/CashbackHackServiceTest.java
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -14,27 +14,22 @@ public class CashbackHackServiceTest {
     public void remain1Test() {
         Assert.assertEquals(service.remain(0), 1000);
     }
-
     @Test
     public void remain2Test() {
         Assert.assertEquals(service.remain(1), 999);
     }
-
     @Test
     public void remain3Test() {
         Assert.assertEquals(service.remain(100), 900);
     }
-
     @Test
     public void remain7Test() {
         Assert.assertEquals(service.remain(900), 100);
     }
-
     @Test
     public void remain4Test() {
         Assert.assertEquals(service.remain(999), 1);
     }
-
     @Test
     public void remain6Test() {
         Assert.assertEquals(service.remain(500), 500);
